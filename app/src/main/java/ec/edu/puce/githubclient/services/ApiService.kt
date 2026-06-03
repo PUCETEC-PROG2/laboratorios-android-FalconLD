c5f5fa43651094ebe49fb2001034069e4a7c2b37
FalconLD <leodavid_a@hotmail.com>
2026-06-03 12:36:14 -0500

    Laboratorio 2: integración con API de GitHub para listar repositorios (Retrofit GET)

---
+++ app/src/main/java/ec/edu/puce/githubclient/services/ApiService.kt
@@ -0,0 +1,28 @@
+package ec.edu.puce.githubclient.services
+
+import ec.edu.puce.githubclient.models.Repository
+import ec.edu.puce.githubclient.models.RepositoryPayload
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("users/{username}/repos")
+    suspend fun getRepositories(
+        @Path("username") username: String,
+        @Query(value = "sort") created : String = "created",
+        @Query(value = "direction") direction : String = "desc",
+        @Query(value = "affiliation") affiliation : String = "owner",
+        @Query(value = "per_page") perPage: Int = 100,
+        @Query(value = "t") t: String = "${System.currentTimeMillis()}",
+    ): List<Repository>
+
+    @POST( value = "/user/repos")
+    suspend fun createRepository(
+        @Body repository: RepositoryPayload
+    ) : Repository
+}
+
